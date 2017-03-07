c554ad685c235a0be8ee386995f65076bdcb1068
Tilen <prevolnik.tilen@gmail.com>
2017-03-07 12:01:00 +0100

    comments

--- handlers/topics.py
+++ handlers/topics.py
@@ -1,18 +1,35 @@
 from handlers.base import BaseHandler
 from models.topic import Topic
-from google.appengine.api import users
+from models.comment import Comment
+from google.appengine.api import users, memcache
+import uuid
+
 
 
 class TopicCreateHandler(BaseHandler):
     def get(self):
-        return self.render_template("topic_create.html")
+        user = users.get_current_user()
+        csrf_token = str(uuid.uuid4())  # convert UUID to string
+        if user:
+            memcache.add(key=csrf_token, value=user.email(), time=600)
+
+        params = {"csrf_token": csrf_token}
+
+        return self.render_template("topic_create.html", params=params)
     def post(self):
-        title = self.request.get("title")
-        content = self.request.get("content")
 
         user = users.get_current_user()
         if not user:
             return self.write("you are not loged in!")
+
+        csrf_token = self.request.get("csrf_token")
+        mem_token = memcache.get(key=csrf_token)
+
+        if not mem_token or str(mem_token) != user.email():
+            return self.write("You are evil attacker...")
+
+        title = self.request.get("title")
+        content = self.request.get("content")
 
         new_topic = Topic(title=title, content=content, author_email=user.email())
         new_topic.put()
@@ -21,9 +38,33 @@ class TopicCreateHandler(BaseHandler):
 
 class TopicHandler(BaseHandler):
     def get(self, topic_id):
+        user = users.get_current_user()
+        csrf_token = str(uuid.uuid4())  # convert UUID to string
+        memcache.add(key=csrf_token, value=user.email(), time=600)
 
         topic = Topic.get_by_id(int(topic_id))
 
-        params = {"topic": topic}
+        comments = Comment.query(Comment.topic_id == topic.key.id(), Comment.deleted == False).order(Comment.created).fetch()
+
+        params = {"topic": topic, "csrf_token": csrf_token, "comments": comments}
 
         return self.render_template("topic.html", params=params)
+
+    def post(self, topic_id):
+        user = users.get_current_user()
+        if not user:
+            return self.write("you are not loged in!")
+
+        csrf_token = self.request.get("csrf_token")
+        mem_token = memcache.get(key=csrf_token)
+
+        if not mem_token or str(mem_token) != user.email():
+            return self.write("You are evil attacker...")
+
+        topic = Topic.get_by_id(int(topic_id))
+        comment = self.request.get("content")
+
+        new_comment = Comment(content=comment, author_email=user.email(), topic_title=topic.title, topic_id=topic.key.id())
+        new_comment.put()
+
+        return self.redirect_to("topic", topic_id=topic.key.id())
